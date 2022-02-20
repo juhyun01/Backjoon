9006d36292ac850a314f60e5aaa37240a3758560
juhyun01 <98452653+juhyun01@users.noreply.github.com>
2022-02-20 13:15:21 +0900

    Add files via upload

---
+++ 1차원 배열/8958.py
@@ -0,0 +1,12 @@
+n = int(input())
+for _ in range(n):
+    count = 0
+    sum = 0
+    a = list(input())
+    for i in a:
+        if i == 'O':
+            count+=1
+            sum = sum+count
+        else:
+            count = 0
+    print(sum)
