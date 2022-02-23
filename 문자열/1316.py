f95cf59ccc4e680fda744ab4b9d7207f890d88ca
juhyun01 <98452653+juhyun01@users.noreply.github.com>
2022-02-23 19:04:40 +0900

    Add files via upload

---
+++ 문자열/1316.py
@@ -0,0 +1,9 @@
+n = int(input())
+count = n
+for i in range(n):
+    word = input()
+    for i in range(len(word)-1):
+        if word.find(word[i]) > word.find(word[i+1]):
+            count -= 1
+            break
+print(count)
